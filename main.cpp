df4143126f32676bea412b1e2f14b8c3de01396d
Yakov Didenko <kogotto@gmail.com>
2023-05-31 15:40:57 +0400

    Read filenames from "argv"

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <string_view>
 
 #include "work.h"
 
-int main() {
-    work("input", "output");
+namespace {
+
+constexpr int WARNING_WRONG_ARGUMENT_COUNT = 1;
+
+void printHelp() {
+    std::cout << "Usage: freq <input_file> <output_file>" << '\n';
+}
+
+} // namespace
+
+int main(int argc, char**argv) {
+    if (argc != 3) {
+        printHelp();
+        return WARNING_WRONG_ARGUMENT_COUNT;
+    }
+
+    const std::string_view inputFilename = argv[1];
+    const std::string_view outputFilename = argv[2];
+
+    std::cout << "input file name is \"" << inputFilename << '\"' << '\n';
+    std::cout << "output file name is \"" << outputFilename << '\"' << '\n';
 
     return 0;
 }
