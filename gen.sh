4b5b137d0b30d8347fd0e0181d89afd5b8e1694a
Yakov Didenko <kogotto@gmail.com>
2023-06-03 20:25:08 +0400

    Easy switch between debug and release builds

--- gen.sh
+++ gen.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 mkdir -p build;
-cmake -B build/ -S .;
+cmake -B build/ -S . -DCMAKE_BUILD_TYPE=Release;
+# cmake -B build/ -S . -DCMAKE_BUILD_TYPE=Debug;
 ln -s build/compile_commands.json .
