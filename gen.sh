9724e659ce1faee2fd15a52e1ee7af35fea98785
Yakov Didenko <kogotto@gmail.com>
2023-06-04 01:30:29 +0400

    Remove warning while creating soft link for compile_commands

--- gen.sh
+++ gen.sh
@@ -3,4 +3,4 @@
 mkdir -p build;
 cmake -B build/ -S . -DCMAKE_BUILD_TYPE=Release;
 # cmake -B build/ -S . -DCMAKE_BUILD_TYPE=Debug;
-ln -s build/compile_commands.json .
+ln -fs build/compile_commands.json .
